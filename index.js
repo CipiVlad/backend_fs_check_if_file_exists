c52ba64dedbf71a28eb32c69dd9fabf155a8a3a6
CipiVlad <supercoder.cipi@gmail.com>
2022-07-01 00:13:02 +0200

    check2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
+function textToFile(text) {
+  return text;
+}
+
 fs.mkdir(path.join("./Project", "test"), (err) => {
   if (err) {
     return console.error(err);
@@ -9,14 +13,20 @@ fs.mkdir(path.join("./Project", "test"), (err) => {
     if (err) {
       return console.error(err);
     }
-    if (
-      fs.access("./Project/test/newFile.txt", (err) => {
-        if (err) {
-          return console.error(err);
+
+    fs.access("./Project/test/newFile.txt", (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      fs.writeFileSync(
+        "./Project/test/newFile.txt",
+        textToFile("text added to File and overwritten previous text"),
+        (err) => {
+          if (err) {
+            return err;
+          }
         }
-      })
-    ) {
-      return console.log("you made it inside a file");
-    }
+      );
+    });
   });
 });
